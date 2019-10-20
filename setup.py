ede14d8580c12b9a4a74caa5f825ea785b10f8df
Hairong_W <whairong2011@gmail.com>
2019-10-20 16:25:55 -0700

    Update setup.py
    Modified install requires, and change to version 3

--- setup.py
+++ setup.py
@@ -3,20 +3,20 @@ from distutils.core import setup
 setup(
   name = 'onpoint',         # How you named your package folder (MyLib)
   packages = ['onpoint'],   # Chose the same as "name"
-  version = '0.2',      # Start with a small number and increase it with every change you make
+  version = '0.3',      # Start with a small number and increase it with every change you make
   license='MIT',        # Chose a license from here: https://help.github.com/articles/licensing-a-repository
   description = 'NLP question answering service',   # Give a short description about your library
   author = 'Hairong Wang',                   # Type in your name
   author_email = 'whairong2011@gmail.com',      # Type in your E-Mail
   url = 'https://github.com/hairong-wang/OnPoint',   # Provide either the link to your github or to your website
-  download_url = 'https://github.com/hairong-wang/OnPoint/archive/v_02.tar.gz',    # I explain this later on
+  download_url = 'https://github.com/hairong-wang/OnPoint/archive/v_03.tar.gz',    # I explain this later on
   keywords = ['NLP', 'question-answering_service', 'Flask-app'],   # Keywords that define your package best
   install_requires=[            # I get to this in a second
           'pandas',
           'numpy',
           'tensorflow',
-          'absl',
-          'nltk.translate.bleu_score'
+          'absl-py',
+          'nltk'
       ],
   classifiers=[
     'Development Status :: 3 - Alpha',      # Choose either "3 - Alpha", "4 - Beta" or "5 - Production/Stable" as the current state of your package
